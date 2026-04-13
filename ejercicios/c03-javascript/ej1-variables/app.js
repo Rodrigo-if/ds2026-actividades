6e7334796aa14967b4fcf2323e975da284fc4f21
Rodrigo <rfigueroa2040@gmail.com>
2026-04-13 10:47:36 -0300

    Ejercicio 1 - Variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,15 @@
+//Mostrar mensaje
+const nombre = "Rodrigo Figueroa";
+const edad = 24;
+const materia = "Desarrollo de Software";
+
+const mensaje = `Me llamo ${nombre}, tengo ${edad} años y curso ${materia}.`;
+console.log(mensaje);
+
+//Contador
+let contador = 0;
+
+for (i = 0; i < 3; i++) {
+    contador++;
+}
+console.log(`Resultado final del contador: ${contador}`);
